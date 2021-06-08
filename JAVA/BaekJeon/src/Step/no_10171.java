314b32fdd33ed552b3a0c787d43a8ff9325b4860
makgoon <miracle013@naver.com>
2021-06-08 13:55:37 +0900

    no 10171
    https://www.acmicpc.net/problem/10171

---
+++ JAVA/BaekJeon/src/Step/no_10171.java
@@ -0,0 +1,14 @@
+package Step;
+
+public class no_10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
