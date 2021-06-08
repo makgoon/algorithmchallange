cf2a12ee28899dff1d9c47d57247490093ec1098
makgoon <miracle013@naver.com>
2021-06-08 15:09:15 +0900

    Create no_10869.java
    https://www.acmicpc.net/problem/10869

---
+++ JAVA/BaekJeon/src/Step/no_10869.java
@@ -0,0 +1,18 @@
+package Step;
+
+import java.util.Scanner;
+
+	public class no_10869 {
+		public static void main(String[] args) {
+		
+			Scanner sc = new Scanner(System.in);
+			int a, b;
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.printf("%d\n", a + b );
+			System.out.printf("%d\n", a - b );
+			System.out.printf("%d\n", a * b );
+			System.out.printf("%d\n", a / b );
+			System.out.printf("%d", a % b );
+	}
+}
