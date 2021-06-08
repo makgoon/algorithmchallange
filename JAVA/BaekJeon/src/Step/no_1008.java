5676316b987cf4573057fb5db47dd7e75eaaafd8
makgoon <miracle013@naver.com>
2021-06-08 14:58:50 +0900

    Create no_1008.java
    https://www.acmicpc.net/problem/1008

---
+++ JAVA/BaekJeon/src/Step/no_1008.java
@@ -0,0 +1,13 @@
+package Step;
+
+import java.util.Scanner;
+
+public class no_1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double  a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.printf("%10.10f", a / b);
+	}
+}
