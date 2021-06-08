fce0e1be760aac9ab2af12a021ab906ecc3e8b43
makgoon <miracle013@naver.com>
2021-06-08 14:27:09 +0900

    Create no_10998.java
    https://www.acmicpc.net/problem/10998

---
+++ JAVA/BaekJeon/src/Step/no_10998.java
@@ -0,0 +1,13 @@
+package Step;
+
+import java.util.Scanner;
+
+public class no_10998 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a * b);
+	}
+}
