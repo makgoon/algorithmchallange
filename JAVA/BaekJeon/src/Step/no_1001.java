1262af67a5ef9c8cd38e6f26d7677acf6d18adbc
makgoon <miracle013@naver.com>
2021-06-08 14:26:11 +0900

    Create no_1001.java
    https://www.acmicpc.net/problem/1001

---
+++ JAVA/BaekJeon/src/Step/no_1001.java
@@ -0,0 +1,14 @@
+package Step;
+
+import java.util.Scanner;
+
+public class no_1001 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a - b);
+	}
+}
