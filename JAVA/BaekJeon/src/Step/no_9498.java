2ab0ebfce380077eac2ec07161d67e6f7dd0ce1f
makgoon <miracle013@naver.com>
2021-06-09 16:22:06 +0900

    Create no_9498.java
    https://www.acmicpc.net/problem/9498

---
+++ JAVA/BaekJeon/src/Step/no_9498.java
@@ -0,0 +1,29 @@
+package Step;
+
+import java.util.Scanner;
+
+public class no_9498 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+			
+		a = sc.nextInt();
+		
+		if(a >= 90)
+			System.out.println("A");
+		else if (a >= 80)
+			System.out.println("B");
+		else if (a >= 70)
+			System.out.println("C");
+		else if (a >= 60)
+			System.out.println("D");
+		else
+			System.out.println("F");			
+		
+		
+	}
+
+}
