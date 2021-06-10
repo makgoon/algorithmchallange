5926e4c8a5375cf16bd4f2d12157a68413ea5a44
makgoon <miracle013@naver.com>
2021-06-10 23:53:58 +0900

    Create no_10950.java
    https://www.acmicpc.net/problem/10950(not clear)

---
+++ JAVA/BaekJeon/src/Step/no_10950.java
@@ -0,0 +1,24 @@
+package Step;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class no_10950 {
+
+	public static void main(String[] args) {
+		int t;
+		int a;
+		int b;
+		Scanner sc = new Scanner(System.in);
+		
+		t = sc.nextInt();
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		for (int i = 0;  i < t; i++) {
+			System.out.println(a + b);
+			
+		}
+	}
+
+}
