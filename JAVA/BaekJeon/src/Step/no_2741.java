4fc44d0ccf2752ae169de65a45de6008d72327a2
makgoon <miracle013@naver.com>
2021-06-14 23:53:26 +0900

    Create no_2741.java
    https://www.acmicpc.net/problem/2741

---
+++ JAVA/BaekJeon/src/Step/no_2741.java
@@ -0,0 +1,15 @@
+package Step;
+
+import java.util.Scanner;
+
+public class no_2741 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for (int i = 1; i <= a; i ++) {
+			System.out.println(i);
+		}
+	}
+}
